d15cf212895b466b8dd5e9b376337c674227f1e9
john <jkbrinker@gmail.com>
2018-07-18 16:24:11 -0400

    functional but not using assigned format techniques

--- src/TableOfPowers.java
+++ src/TableOfPowers.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 
 public class TableOfPowers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String cont = "y";
+		System.out.println("Learn your squares and cubes!");
+		System.out.println("Enter an integer: ");
+		int userNum = scan.nextInt();
+		
 
+		while (cont.equalsIgnoreCase("y")) {
+
+			System.out.println("Number	Squared	Cubed");
+			System.out.println("======	======	======");
+
+			for (int i = 1; i <= userNum; i++) {
+				System.out.print(i);
+				double userDouble = i;
+				System.out.print("	" + (int) Math.pow(userDouble, 2));
+				System.out.println("	" + (int) Math.pow(userDouble, 3));
+			}
+
+			System.out.println("Continue? (y/n): ");
+			scan.nextLine();
+			cont = scan.nextLine();
+		}
+		scan.close();
 	}
-
 }
